5d503e3bdd47ca4c8c320e69f0ca4742c78ca619
Bryan Lopez <bryanlopezrr@gmail.com>
2020-07-21 14:07:48 -0400

    connecting activities in the ui

--- app/src/main/java/com/example/keepthechange/AccountActivity.java
+++ app/src/main/java/com/example/keepthechange/AccountActivity.java
@@ -1,8 +1,14 @@
 package com.example.keepthechange;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -10,6 +16,40 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+
+
+        BottomNavigationView bottomNavBar = findViewById(R.id.navigationBar);
+        Menu menu = bottomNavBar.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+
+        bottomNavBar.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_reports:
+//                        Toast.makeText(MeditateActivity.this, "Reports!!!!", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(AccountActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_meditate:
+//                        Toast.makeText(MeditateActivity.this, "Meditate!!!!", Toast.LENGTH_LONG).show();
+                        Intent intent1 = new Intent(AccountActivity.this, InvestmentsActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.nav_reminder:
+//                        Toast.makeText(MeditateActivity.this, "Monitor!!!!", Toast.LENGTH_LONG).show();
+                        Intent intent2 = new Intent(AccountActivity.this, AccountActivity.class);
+                        startActivity(intent2);
+                        break;
+                }
+            }
+
+        });
+
 
     }
 }
